08936a50ad4ec2e60d500b38c620eaa0b31cd6af
Ahmed Omran <ahmedomran1950@gmail.com>
2025-08-19 08:50:56 +0300

    fix deploy css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
